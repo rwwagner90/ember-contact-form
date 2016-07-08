6243bf56753c1a08f2403bd79421a6e92067dd2d
Robert Wagner <rwwagner@vt.edu>
2016-07-07 22:13:24 -0400

    Move images to vendor

--- index.js
+++ index.js
@@ -5,5 +5,14 @@ module.exports = {
   name: 'ember-contact-form',
   included: function(app) {
     this._super.included(app);
+    app.import('vendor/img/arrow.svg');
+    app.import('vendor/img/budget.svg');
+    app.import('vendor/img/check.svg');
+    app.import('vendor/img/company.svg');
+    app.import('vendor/img/email.svg');
+    app.import('vendor/img/message.svg');
+    app.import('vendor/img/radio.svg');
+    app.import('vendor/img/required.svg');
+    app.import('vendor/img/user.svg');
   }
 };
