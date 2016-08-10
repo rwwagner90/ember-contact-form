9b1226d1e04d34952502a80e148262eb73c87f3a
Robert Wagner <rwwagner90@gmail.com>
2016-08-10 13:11:18 -0400

    Update index.js

--- index.js
+++ index.js
@@ -16,12 +16,9 @@ module.exports = {
     app.import('vendor/img/user.svg');
   },
   init() {
+    this._super.init && this._super.init.apply(this, arguments);
     this.options = this.options || {};
     this.options.babel = this.options.babel || {};
-    this.options.babel.optional = this.options.babel.optional || [];
-
-    if (this.options.babel.optional.indexOf('es7.decorators') === -1) {
-      this.options.babel.optional.push('es7.decorators');
-    }
+    this.options.babel.stage = 0;
   }
 };
