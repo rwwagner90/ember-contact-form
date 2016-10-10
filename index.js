e3d43e8819f6b1c1547f90767955c7672a2af863
Robert Wagner <rwwagner@vt.edu>
2016-10-10 19:39:58 -0400

    Fix ESLint parsing issue

--- index.js
+++ index.js
@@ -17,8 +17,13 @@ module.exports = {
   },
   init() {
     this._super.init && this._super.init.apply(this, arguments);
+
     this.options = this.options || {};
     this.options.babel = this.options.babel || {};
-    this.options.babel.stage = 0;
+    this.options.babel.optional = this.options.babel.optional || [];
+
+    if (this.options.babel.optional.indexOf('es7.decorators') === -1) {
+      this.options.babel.optional.push('es7.decorators');
+    }
   }
 };
