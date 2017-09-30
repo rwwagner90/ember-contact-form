017dfc62096fbc7b02b9a6ac6fe2e2e1412390e9
Robert Wagner <rwwagner90@gmail.com>
2017-09-29 22:22:27 -0400

    Remove truth helpers

--- addon/components/list-item/component.js
+++ addon/components/list-item/component.js
@@ -5,6 +5,8 @@ import layout from './template';
 export default Component.extend({
   layout,
   tagName: 'li',
+  isCheckbox: computed.equal('type', 'checkbox'),
+  isRadio: computed.equal('type', 'radio'),
   id: computed('fieldName', function() {
     let fieldName = this.get('fieldName');
     return `cd-${fieldName}`;
