e9c2c63a69f01848b3c492ed1e4932208032dae0
Robert Wagner <rwwagner90@gmail.com>
2017-11-29 16:37:35 -0500

    Use modules

--- addon/components/list-item/component.js
+++ addon/components/list-item/component.js
@@ -1,12 +1,13 @@
-import Component from 'ember-component';
-import computed from 'ember-computed';
+import Component from '@ember/component';
+import { computed } from '@ember/object';
+import { equal } from '@ember/object/computed';
 import layout from './template';
 
 export default Component.extend({
   layout,
   tagName: 'li',
-  isCheckbox: computed.equal('type', 'checkbox'),
-  isRadio: computed.equal('type', 'radio'),
+  isCheckbox: equal('type', 'checkbox'),
+  isRadio: equal('type', 'radio'),
   id: computed('fieldName', function() {
     let fieldName = this.get('fieldName');
     return `cd-${fieldName}`;
