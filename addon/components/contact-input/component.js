f98a3823e57646ab815fa32aac298aa44df16fa7
Robert Wagner <rwwagner90@gmail.com>
2018-02-17 16:27:00 -0500

    Use computed instead of truth helpers

--- addon/components/contact-input/component.js
+++ addon/components/contact-input/component.js
@@ -1,12 +1,18 @@
 import Component from '@ember/component';
 import { computed } from '@ember/object';
+import { equal } from '@ember/object/computed';
 import layout from './template';
 
 export default Component.extend({
   layout,
   tagName: 'div',
-  classNameBindings: [':contact-item', ':icon', 'textarea:message'],
+  classNames: ['contact-item', 'icon'],
+  classNameBindings: ['textarea:message'],
+
   textarea: false,
+
+  isEmail: equal('type', 'email'),
+
   idAndName: computed('fieldName', function() {
     let fieldName = this.get('fieldName');
     return `cd-${fieldName}`;
